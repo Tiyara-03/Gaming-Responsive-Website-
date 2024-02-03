3835f06bd0f17f506193eb6f47f1a820a42bfb1c
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-03 19:40:09 +0530

    js

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("hello world");
+
+
+function togglehide(){
+    let menu = document.getElementById("menu-icon")
+    let navlist = document.getElementById("navlist");
+
+    if(navlist.style.display != 'none'){
+        navlist.style.display = 'none';
+    }
+    else{
+        navlist.style.display = 'block';
+    }
+}
+
+const sr= ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+})
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
+sr.reveal('.scroll-down',{delay:500, origin:'right'});
+
+// let menu = document.querySelector('#menu-icon');
+// let navlist = document.querySelector('.navlist');
+
+// menu.onclick =()=>{
+//     menu.classList.toggle('bx-x')
+//     navlist.classList.toggle('open');
+// }
+
